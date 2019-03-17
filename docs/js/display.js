4c3dad9dcdc068a68a7a55b665b1f416af09a24e
myeongsoo.shin <myeongsoo.shin@naverlabs.com>
2019-03-17 14:10:52 +0900

    responsive layout

--- docs/js/display.js
+++ docs/js/display.js
@@ -1,9 +1,63 @@
+const wThreshold = 480;
+var screen_state;
+var screen_state_old;
+var SCREEN_ENUM = Object.freeze({
+	PC: 0,
+	TABLET: 1,
+	MOBILE: 2,
+});
+var screen_name = ['pcLandscape', 'pcPortrait', 'mobilePortrait'];
+/*
+var columnVal = {
+  'pcLandscape': {
+    'number': [3,2,3,3,2,3],
+    'height': [],
+  },
+  'pcPortrait': {
+    'number': [4,4,4,4],
+    'height': [],
+  },
+  'mobilePortrait': {
+    'number': [8,8],
+    'height': [],
+  },
+};
+*/
+var columnVal = [
+  {
+    'number': [3,2,3,3,2,3],
+    'height': [],
+  },
+  {
+    'number': [4,4,4,4],
+    'height': [],
+  },
+  {
+    'number': [8,8],
+    'height': [],
+  }
+];
+
 $(document).on('contextmenu', function(){
 	return false;
 });
 
 $(document).ready(function(){
   setCSS();
+  /*
+  setColumn(columnVal.pcLandscape);
+  setColumn(columnVal.pcPortrait);
+  setColumn(columnVal.mobilePortrait);
+  */
+  $(columnVal).each(function(i, item){
+    setColumn(columnVal[i]);
+  });
+  putCell();
+  changeHeight();
+});
+
+$(window).resize(function(){
+  changeHeight();
 });
 
 function setCSS(){
@@ -16,4 +70,82 @@ function setCSS(){
 		//console.log('pc browser');
 		$("head").append("<link rel='stylesheet' href='style-pc.css' type='text/css' media='screen' />");
 	}
+}
+
+function putCell(){
+  $(index).each(function(i, item){
+    var cell = $('<div></div>');
+    $(cell).addClass('cell');
+    //$(cell).height(columnVal.pcLandscape.height[i]);
+    $('.liner').append(cell);
+  });
+}
+function changeHeight(){
+  var wW = $(window).width();
+  var wH = $(window).height();
+  var wR = wW/wH;
+  if(wW > wThreshold){
+    if(wR > 1){//pc
+      screen_state = SCREEN_ENUM.PC;
+    }
+    else{//tablet
+      screen_state = SCREEN_ENUM.TABLET;
+    }
+  }
+  else{//mobile
+    screen_state = SCREEN_ENUM.MOBILE;
+  }
+  /* check difference */
+  if(screen_state_old == null){//put default value
+    screen_state_old = screen_state;
+    setHeight(screen_state);
+  }
+  else {//compare old value
+    if(screen_state != screen_state_old){
+      setHeight(screen_state);
+      screen_state_old = screen_state;
+    }
+  }
+}
+
+
+
+function setHeight(state){
+  var heightValue = columnVal[state].height;
+  $('.cell').each(function(i, item){
+    $(item).height(heightValue[i]);
+    console.log(state + '/' + heightValue[i]);
+  });
+}
+
+
+
+function setColumn(column){
+  var heightArray = [];
+  $(column.number).each(function(i, item){
+    switch(item){
+      case 2:
+        var temp = 40 + 20 * Math.random();
+        heightArray.push(temp + '%');
+        heightArray.push(100 - temp + '%');
+        break;
+      case 3:
+        var temp1 = 25 + 12.5 * Math.random();
+        var temp2 = 25 + 12.5 * Math.random();
+        heightArray.push(temp1 + '%');
+        heightArray.push(temp2 + '%');
+        heightArray.push(100 - (temp1 + temp2) + '%');
+        break;
+      case 4:
+        var temp1 = 20 + 8 * Math.random();
+        var temp2 = 20 + 8 * Math.random();
+        var temp3 = 20 + 8 * Math.random();
+        heightArray.push(temp1 + '%');
+        heightArray.push(temp2 + '%');
+        heightArray.push(temp3 + '%');
+        heightArray.push(100 - (temp1 + temp2 + temp3) + '%');
+        break;
+    }
+  });
+  column.height = heightArray;
 }
---
+++ docs/js/meta.js
@@ -0,0 +1,82 @@
+var index = [
+  {
+    'name': 'portfolio',
+    'url': 'http://myeongsoo-shin.com',
+    'description': 'Design Portfolio of Professional Work',
+  },
+  {
+    'name': 'zommeway',
+    'url': 'http://zommeway.zomme.net',
+    'description': 'some footprints left on Jirisan Dullegil trail',
+  },
+  {
+    'name': 'zommeone',
+    'url': 'http://zommeone.zomme.net',
+    'description': 'life, thoughts, and monologue',
+  },
+  {
+    'name': 'zommewhere',
+    'url': 'http://zommewhere.zomme.net',
+    'description': 'the trace of somewhere I have been',
+  },
+  {
+    'name': 'zommeday',
+    'url': 'http://zommeday.zomme.net',
+    'description': 'doodles drawn to remember some days in my life',
+  },
+  {
+    'name': 'zommetimes',
+    'url': 'http://zommetimes.zomme.net',
+    'description': 'slices of a film named as "memory"',
+  },
+  {
+    'name': 'facebook',
+    'url': 'https://www.facebook.com/zommestyle',
+    'description': 'facebook of Myeong Shin, a.k.a. zomme',
+  },
+  {
+    'name': 'instragram',
+    'url': 'https://www.instagram.com/zommeywalker',
+    'description': 'zommeywalker, a loner who walks backward',
+  },
+  {
+    'name': 'dialect workbook',
+    'url': 'https://dialectworkbook.wordpress.com',
+    'description': 'a blog for Gyeongsoang-do dialect workbook',
+  },
+  {
+    'name': 'daily emotion',
+    'url': '',
+    'description': 'traces of emotions',
+  },
+  {
+    'name': 'zommehow',
+    'url': '',
+    'description': '',
+  },
+  {
+    'name': 'zommewhat',
+    'url': '',
+    'description': '',
+  },
+  {
+    'name': 'zommehow',
+    'url': '',
+    'description': '',
+  },
+  {
+    'name': 'zommewhat',
+    'url': '',
+    'description': '',
+  },
+  {
+    'name': 'zommehow',
+    'url': '',
+    'description': '',
+  },
+  {
+    'name': 'zommewhat',
+    'url': '',
+    'description': '',
+  },
+];
